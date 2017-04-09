a1fa06dd0781c482024af3f929cd7ba0587dc9ae
jack <jackhd@vip.qq.com>
2017-04-09 15:39:53 +0800

    add some code to conflict with master

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
-int main() {
+void f() {
 	std::cout << "hello git!" << std::endl;
+}
+
+int main() {
+	std::cout << "this is a git test" << std::endl;
+	int i = 0;
+	while (i < 10) {
+		f();
+		++i;	
+	}
 	return 0;
 }
